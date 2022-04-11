ba2218f53b448da4dbe4ec3404c981976674d525
Robbi Coronel <65572131+RobbiCoronel@users.noreply.github.com>
2022-04-11 15:42:54 -0500

    update servidor

--- servidor1.py
+++ servidor1.py
@@ -1,15 +1,30 @@
-import socket 
-HOST=""
-PORT=1234
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    print("esperando conexion")
-s.bind(HOST,PORT)
-s.listen()
-conn,addr=s.accept()
-with conn:
-    print(f"Conectado al cliente{addr}")
+from socket import *
+
+
+HOST = ''
+PORT = 1234
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)#Inicialización
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen()# Puerto de escucha
+
+
+while True:
+    print('witing for connection')#Esperando la conexión del cliente
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('connected from:{}'.format(addr))
+
     while True:
-        data=conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
+        data = tcpCliSock.recv(BUFSIZ).decode()# Personajes aceptados
+        if data:
+                print('recibido: {}'.format(data.encode('utf-8')))
+        else:
+                print("Sin conexion de cliente")
+                break
+        mensaje = "Hola desde el servidor"
+        tcpCliSock.send(mensaje.encode())# Enviar personaje
+    tcpCliSock.close()
+tcpSerSock.close()#Cerrar puerto
+
