a947ff9c4b4c493a20139c7b9ad36164980c85c9
Robbi Coronel <65572131+RobbiCoronel@users.noreply.github.com>
2022-04-12 22:52:48 -0500

    Actualización cliente multiusarios

--- cliente3.py
+++ cliente3.py
@@ -1,30 +1,23 @@
-import socket
+import socket #libreria para trabajar con sockets
 
-host = "127.0.0.1"
-port = 6666
+host = "172.17.37.152" #declaracion de la ip del servidor
+port = 6666 #declaracion del puerto
 
-sock = socket.socket()
+sock = socket.socket() #objeto socket no bloqueante
 
-sock.connect((host, port))
+sock.connect((host, port)) # Conecta el programa del servidor
 
-datos = sock.recv(4096)
-print (datos.decode('utf-8'))
+datos = sock.recv(4096) #aceptar el mensaje del servidor
+print (datos.decode('utf-8')) #imprime el mensaje
 
-
-
+#Enviar mensaje
 while True:
 
-
   message = input("envia un mensaje: ")
-  sock.send(message.encode('utf-8'))
-
-
-
+  sock.send(message.encode('utf-8')) #envia el mensaje al servidor en bytes
 
   if message == "salir":
-        print("bye")
-        
-        sock.close()
-        break
+        print("bye") 
+        break #sale del bucle
 
-
+sock.close() #finalizacion del socket
