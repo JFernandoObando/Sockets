2352770cd58dc2b8d071e5cb614db673dc25e820
Robbi Coronel <65572131+RobbiCoronel@users.noreply.github.com>
2022-04-12 22:50:40 -0500

    Actualización cliente tcp

--- cliente1.py
+++ cliente1.py
@@ -1,21 +1,21 @@
-from socket import *
+from socket import * #libreria para trabajar con sockets
 
-HOST = '172.17.37.152'
-PORT = 1236
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
+HOST = '172.17.37.152' #declaracion de la ip del servidor
+PORT = 1236 #declaracion del puerto
+BUFSIZ = 1024 #tamaño del bufer en bytes
+ADDR = (HOST, PORT) 
 
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
-tcpCliSock.connect(ADDR)
+s_tcp = socket(AF_INET, SOCK_STREAM) #Creacion de socket con parametros de TCP
+s_tcp.connect(ADDR)# Conecta el programa del servidor
 
+#Enviar y recibir mensajes
 while True:
-    sdata = input('> ')
-    tcpCliSock.send(sdata.encode('utf-8'))
-    if not sdata:
-        break
-    tcpCliSock.send(sdata.encode())
-    rdata = tcpCliSock.recv(BUFSIZ).decode()
-    if not rdata:
-        break
-    print(rdata)
-tcpCliSock.close()
+    dato = input('Ingresa un mensaje: ')
+    if not dato:
+        break #dato vacio sale del bucle
+    s_tcp.send(dato.encode('utf-8')) #envia el mensaje al servidor en bytes
+    b_dato = s_tcp.recv(BUFSIZ).decode() #aceptar el mensaje del servidor
+    if not b_dato:
+        break #sin bytes sale del bucle
+    print(b_dato) #imprime el mensaje del servidor
+s_tcp.close() #finalizacion del socket
