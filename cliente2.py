4ad30906d52aff3a08268b230f6d4772c418a3e8
Robbi Coronel <65572131+RobbiCoronel@users.noreply.github.com>
2022-04-12 22:51:36 -0500

    Actualización de cliente udp

--- cliente2.py
+++ cliente2.py
@@ -1,18 +1,18 @@
+import socket #libreria para trabajar con sockets
+ 
+s_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Creacion de socket con parametros de UDP
+addr = ("172.17.37.152", 55857) # Conecta el programa del servidor con la ip y puerto
 
-import socket
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-addr = ("127.0.0.1", 55857)
- 
+#Enviar y recibir mensajes
 while True:
     data = input("Ingresa tu nombre: ")
     if not data:
-        continue
-    s.sendto(data.encode(), addr)
-    response, addr = s.recvfrom(1024)
-    print(response.decode())
+        continue 
+    s_udp.sendto(data.encode(), addr) #envia el mensaje al servidor en bytes
+    response, addr = s_udp.recvfrom(1024) #aceptar el mensaje del servidor
+    print(response.decode()) #imprime el mensaje escrito
     if data == "salir":
         print("La sesion con el servidor a terminado %s:%s\n" % addr)
-        break
- 
-s.close()
+        break #sale del bucle
+
+s_udp.close() #finalizacion del socket
