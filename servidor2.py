c20145f91ea9e834a651a8ced486e81ad1b64784
Juan Fernando Obando <67531483+JFernandoObando@users.noreply.github.com>
2022-04-13 17:18:38 -0500

    4to Commit Servidor 2

--- servidor2.py
+++ servidor2.py
@@ -1,10 +1,15 @@
+#Importar la libreria para usar sockets
 import socket
- 
+
+#Inicializacion del socket de tipo UDP
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Definir el puerto por el cual va a escuchar
 PORT=55857
+#Asociar el puerto y el host con el socket
 s.bind(("", PORT))
+#Mensaje de prueba 
 print("Conexion UDP desde el puerto",PORT,".....")
- 
+#Crear una comunicacion entre servidor y cliente 
 while True:
     data, addr = s.recvfrom(1024)
     print("Recibido desde:  %s:%s" % addr)
